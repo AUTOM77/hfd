3d270efeac2523c11f4a16b4f308d179abd7c698
Monius <m1105930979@gmail.com>
2024-05-05 19:56:08 +0800

    v0.1.5 tokio_stream

--- hfd-cli/src/cli.rs
+++ hfd-cli/src/cli.rs
@@ -1,11 +1,12 @@
 use tokio;
+use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() {
     let api = libhfd::api::tokio::Api::new().unwrap();
 
     let _filename = api
-        .model("ByteDance/Hyper-SD".to_string())
+        .dataset("ByteDance/Hyper-SD".to_string())
         .get("Hyper-SDXL-8steps-lora.safetensors")
         .await
         .unwrap();
